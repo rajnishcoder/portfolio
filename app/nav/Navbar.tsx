74694ba69d53c5e6d2a48367eb907c4f71c9a38a
Rajnish <getwere@gmail.com>
2024-02-13 13:01:32 +0530

    setting upi things

---
+++ app/nav/Navbar.tsx
@@ -0,0 +1,16 @@
+
+
+export default function NavBar() {
+
+    return (
+        <div className="flex">
+            <div className="w-20 h-20 bg-slate-50">test</div>
+            <ul>
+                <li>About</li>
+                <li>Exprience</li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    )
+}
